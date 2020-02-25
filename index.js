6d3dbef33d12630a32e2c406a3b8c015da766c6d
kjdowns <kevin.downs04@gmail.com>
2020-02-25 16:49:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function fetchBooks() {
-
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(function(response) {
+  return response.json();
+  })
+  .then(function(json){
+    renderBooks(json);
+    renderFifthBook(json);
+  })
 }
 
 function renderBooks(json) {
@@ -9,6 +16,13 @@ function renderBooks(json) {
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
   })
+}
+
+function renderFifthBook(json) {
+  const main = document.querySelector('main')
+  const h1 = document.createElement('h1')
+  h1.innerText = `Book Five: ${json[4]["name"]}`
+  main.appendChild(h1)
 }
 
 document.addEventListener('DOMContentLoaded', function() {
